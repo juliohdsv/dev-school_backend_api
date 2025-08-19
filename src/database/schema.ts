cd0a0ba50f25f1203cdab8cd38bea8ef7bf64ca8
juliohdsv <ricksv92@gmail.com>
2025-08-19 20:36:45 -0300

    fix(Schema.ts): Fixed the user model

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,18 +1,21 @@
-import { 
-  pgTable, 
-  uuid, 
-  text, 
-  timestamp, 
-  uniqueIndex 
-} from "drizzle-orm/pg-core";
+import { uniqueIndex } from 'drizzle-orm/pg-core'
+import { timestamp } from 'drizzle-orm/pg-core'
+import { pgTable, uuid, text, pgEnum } from 'drizzle-orm/pg-core'
 
-export const users = pgTable("users", {
+export const userRole = pgEnum('user_role', [
+  'student',
+  'manager'
+])
+
+export const users = pgTable('users', {
   id: uuid().primaryKey().defaultRandom(),
   name: text().notNull(),
   email: text().notNull().unique(),
+  password: text().notNull(),
+  role: userRole().notNull().default('student'),
 })
 
-export const courses = pgTable("courses",{
+export const courses = pgTable('courses', {
   id: uuid().primaryKey().defaultRandom(),
   title: text().notNull().unique(),
   description: text(),
